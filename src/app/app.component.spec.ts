a0f72d4f24571233c3d06094a266a5ee72fd9f7e
nardos111 <nardos2alemu@gmail.com>
2022-03-08 01:13:37 -0500

    Restart project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'to-do'`, () => {
+  it(`should have as title 'task-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('to-do');
+    expect(app.title).toEqual('task-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('to-do app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('task-tracker app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ButtonComponent } from './button/button.component';
-import { CardComponent } from './card/card.component';
-import { AlertModule } from 'ngx-bootstrap/alert' 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ButtonComponent,
-    CardComponent
+    AppComponent
   ],
   imports: [
-    AlertModule.forRoot(),
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
