dd16e001637179c22c7df30ae533873480f37fe4
nardos111 <nardos2alemu@gmail.com>
2022-03-08 19:12:32 -0500

    Reminder toggle

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,6 +3,12 @@ import {Task} from '../Tasks';
 import {Observable} from 'rxjs'
 import {HttpClient, HttpHeaders} from '@angular/common/http'
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,5 +23,10 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task>{
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url)
+  }
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task)
+
   }
 }
